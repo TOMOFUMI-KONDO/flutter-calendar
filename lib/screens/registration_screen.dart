9d768aa50e8e245c22629ae828de5d11600d59e8
alan <g1953788@gmail.com>
2020-09-25 21:15:23 +0900

    登録画面の追加

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar/screens/homepage_screen.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
+  static String id = 'registration_screen';
+
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
-//  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseAuth _auth = FirebaseAuth.instance;
   bool showSpinner = false;
   String email;
   String password;
@@ -57,20 +60,20 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                     showSpinner = true;
                   });
 
-//                  try {
-//                    final newUser = await _auth.createUserWithEmailAndPassword(
-//                        email: email, password: password);
-//
-//                    if (newUser != null) {
-//                      print('succeeded');
-//                    }
-//                  } on FirebaseAuthException catch (e) {
-//                    if (e.code == 'user-not-found') {
-//                      print('No user found for that email.');
-//                    } else if (e.code == 'wrong-password') {
-//                      print('Wrong password provided for that user.');
-//                    }
-//                  }
+                  try {
+                    final newUser = await _auth.createUserWithEmailAndPassword(
+                        email: email, password: password);
+
+                    if (newUser != null) {
+                      Navigator.pushNamed(context, HomepageScreen.id);
+                    }
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'user-not-found') {
+                      print('No user found for that email.');
+                    } else if (e.code == 'wrong-password') {
+                      print('Wrong password provided for that user.');
+                    }
+                  }
                 },
               ),
             ],
