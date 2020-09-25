2561452d2293979f70a1d94ee4a423b25991b27f
alan <g1953788@gmail.com>
2020-09-25 21:29:07 +0900

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_calendar/screens/MyHomePage_screen.dart';
+import 'package:flutter_calendar/screens/homepage_screen.dart';
+import 'package:flutter_calendar/screens/login_screen.dart';
+import 'package:flutter_calendar/screens/registration_screen.dart';
+import 'package:flutter_calendar/screens/schedule_screen.dart';
 import 'constants.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,7 +20,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: themePrimarySwatch,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: RegistrationScreen.id,
+      routes: {
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ScheduleScreen.id: (context) => ScheduleScreen(),
+        HomepageScreen.id: (context) => HomepageScreen(),
+      },
     );
   }
 }
